39e3c539adb06975f26828e3ac9c67a58d446757
George Roman <george.roman.99@gmail.com>
2022-07-31 17:46:45 +0300

    feat: allow specifying gas fields and balance overrides

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,8 @@
 import { BigNumber, BigNumberish } from "@ethersproject/bignumber";
+import { hexValue } from "@ethersproject/bytes";
 
 export const bn = (value: BigNumberish) => BigNumber.from(value);
+
+export const hex = (value: BigNumberish) => hexValue(bn(value).toHexString());
 
 export const getSelector = (calldata: string) => calldata.slice(0, 10);
